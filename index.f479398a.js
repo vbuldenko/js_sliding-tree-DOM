3ab6f1092ea8b15c85020b8521ee13245f7a8863
Vladyslav <vladbuldenko@gmail.com>
2024-06-09 23:56:44 +0300

    make build

---
+++ index.f479398a.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".tree");document.querySelectorAll("li").forEach(function(e){var n=document.createElement("span");e.prepend(n),n.append(n.nextSibling)}),e.addEventListener("click",function(e){var n=e.target.nextElementSibling;n.hidden=!n.hidden});
+//# sourceMappingURL=index.f479398a.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,14 @@
+"use strict";
+const tree = document.querySelector(".tree");
+const list = document.querySelectorAll("li");
+list.forEach((li)=>{
+    const span = document.createElement("span");
+    li.prepend(span);
+    span.append(span.nextSibling);
+});
+tree.addEventListener("click", (e)=>{
+    const ul = e.target.nextElementSibling;
+    ul.hidden = !ul.hidden;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
